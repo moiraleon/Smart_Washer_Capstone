bace20358d3556235fde2d55ed53e056908a5d91
Moira Leon <moira.leon2@gmail.com>
2021-10-07 03:11:46 -0600

    pseudo code for preventing duplicate orders and reset fill level

--- src/main/java/com/database/SmartWasherJDBC/ReorderDetergent.java
+++ src/main/java/com/database/SmartWasherJDBC/ReorderDetergent.java
@@ -1,48 +1,52 @@
 package com.database.SmartWasherJDBC;
 
+import javax.swing.*;
+
 public class ReorderDetergent {
-    private boolean unordered;
-    private boolean purchased;
-    private boolean orderUnsuccessful;
-    private boolean inTransit;
+    private boolean orderInProcess;
+    private boolean orderNotInProcess;
+
 
 
     public ReorderDetergent( boolean unordered, boolean purchased,boolean orderUnsuccessful, boolean inTransit){
-        this.unordered = unordered;
-        this.purchased =purchased;
-        this.orderUnsuccessful =orderUnsuccessful;
-        this.inTransit = inTransit;
+        this.orderInProcess = orderInProcess;
+        this.orderNotInProcess =orderNotInProcess;
+
     }
 
-    public boolean isUnordered() {
-        return unordered;
+    public boolean isOrderInProcess() {
+        return orderInProcess;
     }
 
-    public void setUnordered(boolean unordered) {
-        this.unordered = unordered;
+    public void setOrderInProcess(boolean orderInProcess) {
+        this.orderInProcess = orderInProcess;
     }
 
-    public boolean isPurchased() {
-        return purchased;
+    public boolean isOrderNotInProcess() {
+        return orderNotInProcess;
     }
 
-    public void setPurchased(boolean purchased) {
-        this.purchased = purchased;
+    public void setOrderNotInProcess(boolean orderNotInProcess) {
+        this.orderNotInProcess = orderNotInProcess;
     }
+    //for reset button to be toggled when soap recieved
+//    public void resetOrderStatus(boolean orderInProcess, boolean orderNotInProcess) extends JFrame implements void ActionListener {
+//        //set orderInProcess to false
+//        //set orderNotInProcess to true
+//        }
+//
+//    }
 
-    public boolean isOrderUnsuccessful() {
-        return orderUnsuccessful;
-    }
+//    public void preventDuplicateOrders(boolean orderInProcess, boolean orderNotInProcess){
+//
+//        if (orderInProcess == true && fillLevel <=8){
+//            //do nothing
+//
+//        }else if(orderNotInProcess == true && fill level <=8){
+//          //set orderInProcess to true and alert user order was placed//not actually ordering at this time
+//
+//        }
+//
+//    }
 
-    public void setOrderUnsuccessful(boolean orderUnsuccessful) {
-        this.orderUnsuccessful = orderUnsuccessful;
-    }
-
-    public boolean isInTransit() {
-        return inTransit;
-    }
-
-    public void setInTransit(boolean inTransit) {
-        this.inTransit = inTransit;
-    }
 }
