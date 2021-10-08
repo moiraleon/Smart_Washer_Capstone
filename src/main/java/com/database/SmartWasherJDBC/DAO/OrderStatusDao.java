b1f38714d0be7861d9bcbbc7681f9dffdae068ac
Moira Leon <moira.leon2@gmail.com>
2021-10-07 21:29:07 -0600

    creating order reset method

--- src/main/java/com/database/SmartWasherJDBC/DAO/OrderStatusDao.java
+++ src/main/java/com/database/SmartWasherJDBC/DAO/OrderStatusDao.java
@@ -1,6 +1,31 @@
 package com.database.SmartWasherJDBC.DAO;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
 public class OrderStatusDao {
 
+    public void resetOrderStatus(Boolean orderPlaced, Boolean orderFulfilled){
+
+        try(
+                Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/devices-data?useSSL=false", "root", "pluralsight");
+                PreparedStatement statement = connection.prepareStatement("INSERT INTO device_one (order_placed,order_fulfilled) VALUES('false','true');")//is this how I would reset them?
+
+
+
+        ){
+            statement.setBoolean(1,orderPlaced);
+            statement.setBoolean(2,orderFulfilled);
+            statement.executeUpdate();
+
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+        return;
+
+    }
 
 }
